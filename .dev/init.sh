e8b0912b03d22f57e849329a3ba81f58d51f35b9
Mao <m@maowtm.org>
2024-09-22 01:19:23 +0100

    --- local tooling ---

---
+++ .dev/init.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+if [ `id -u` -ne 0 ]; then
+    echo "Don't run this outside the VM..."
+    exit 1
+fi
+
+set -xe
+export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
+export HOME=/
+export LC_CTYPE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export USER=root
+mkdir -p /dev /proc /sys /tmp /sys /mnt
+mount -t devtmpfs dev /dev
+mount -t proc proc /proc
+mount -t sysfs sys /sys
+mount -t tmpfs tmp /tmp
+mkdir -p /dev/pts /dev/shm /dev/hugepages /dev/mqueue
+mount -t devpts devpts /dev/pts
+mount -t tmpfs shm /dev/shm
+mount -t hugetlbfs hugetlbfs /dev/hugepages
+mount -t mqueue mqueue /dev/mqueue
+mkdir -p /sys/kernel/security /sys/fs/cgroup /sys/fs/bpf /sys/kernel/tracing
+mount -t securityfs none /sys/kernel/security
+mount -t cgroup2 none /sys/fs/cgroup
+mount -t bpf none /sys/fs/bpf
+mount -t tracefs none /sys/kernel/tracing
+pushd /sys/kernel/tracing
+echo 1 > events/printk/enable
+popd
+mkdir /tmp/run_
+mount -t tmpfs none /tmp/run_
+cp -a /run/* /tmp/run_
+mount --move /tmp/run_ /run
+rmdir /tmp/run_
+ln -s /proc/self/fd /dev/fd
+ln -s /proc/self/fd/0 /dev/stdin
+ln -s /proc/self/fd/1 /dev/stdout
+ln -s /proc/self/fd/2 /dev/stderr
+hostname -F /etc/hostname
+. /_runtime_init.sh
+uname -a
+ethName=eth0
+if [ -e "/sys/class/net/$ethName" ]; then
+    # Set up network - because dhcpcd is quite slow we just use static IP (hard-coded in startvm.sh)
+    ip link set dev $ethName up
+    ip addr add 10.0.0.2/24 dev $ethName
+    ip route add default via 10.0.0.1 dev $ethName
+    mkdir -p /run/sshd
+    /usr/sbin/sshd
+fi
+if [ -e /dev/vda ]; then
+    mount /dev/vda /mnt
+fi
+set +ex
+exec_args="${@:2}" # Remove the `-`
+if [ -z "$exec_args" ]; then
+    /bin/bash || true
+else
+    eval "$exec_args"
+    ret=$?
+    if [ $ret -ne 0 ]; then
+        echo "Command exited with code $ret"
+    fi
+    /bin/bash || true
+fi
+set -ex
+umount /mnt
+sync
+echo o > /proc/sysrq-trigger
+sleep infinity
