573dc4e7b92eec9e01be62f1f659f699241f0ffa
Mao <m@maowtm.org>
2025-01-04 20:36:12 +0000

    Move struct defines into CONFIG_ICK condition

--- include/linux/ick.h
+++ include/linux/ick.h
@@ -6,6 +6,7 @@
 #include <linux/types.h>
 #include <linux/sched.h>
 
+#ifdef CONFIG_ICK
 struct ick_checked_process {
   struct thread_struct saved_state;
   struct pt_regs saved_regs;
@@ -22,7 +23,6 @@ struct ick_modified_page {
   u8 *orig_page_content;
 };
 
-#ifdef CONFIG_ICK
 int ick_checkpoint_proc(void);
 int ick_revert_proc(bool reset_ick);
 vm_fault_t ick_do_wp_page(struct vm_fault *vmf);
