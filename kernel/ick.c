d20b725405f62e7f8127a8880d03ca9199f7b3d3
Tingmao Wang <m@maowtm.org>
2025-04-17 16:07:27 +0100

    Copying pages

--- kernel/ick.c
+++ kernel/ick.c
@@ -4,6 +4,7 @@
 #include <linux/errno.h>
 #include <linux/ptrace.h>
 #include <linux/sched/task_stack.h>
+#include <linux/vmalloc.h>
 #include <asm/tlb.h>
 
 // Mark pages as write-protected
@@ -55,6 +56,10 @@ vm_fault_t ick_do_wp_page(struct vm_fault *vmf) {
 	unsigned long page_addr = vmf->address;
 	struct task_struct *task = current;
 	struct ick_checked_process *ick_data = task->ick_data;
+	struct ick_modified_page *mod_page;
+	struct rb_node **new;
+	struct rb_node *parent = NULL;
+	long ret;
 
 	BUG_ON(!ick_data);
 	BUG_ON(!(vmf->flags & FAULT_FLAG_WRITE));
@@ -62,7 +67,44 @@ vm_fault_t ick_do_wp_page(struct vm_fault *vmf) {
 	trace_printk("CoWing page 0x%px following wp fault at offset 0x%x\n",
 					(void *)page_addr, (int)(vmf->real_address - page_addr));
 
-	/* TODO */
+	spin_lock(&ick_data->tree_lock);
+	new = &ick_data->modified_pages_tree.rb_node;
+	while (*new) {
+		parent = *new;
+		mod_page = rb_entry(parent, struct ick_modified_page, node);
+
+		if (page_addr < mod_page->addr)
+			new = &parent->rb_left;
+		else if (page_addr > mod_page->addr)
+			new = &parent->rb_right;
+		else {
+			/* Page already in tree, so it's already copied before. Ignore. */
+			trace_printk("Already in tree page 0x%px hti wp fault again\n", (void*)page_addr);
+			spin_unlock(&ick_data->tree_lock);
+			return 0;
+		}
+	}
+
+	u8 *copied_page_content = vmalloc(PAGE_SIZE);
+	if (!copied_page_content) {
+		spin_unlock(&ick_data->tree_lock);
+		return VM_FAULT_OOM;
+	}
+
+	mod_page = vmalloc(sizeof(*mod_page));
+	mod_page->addr = page_addr;
+	rb_link_node(&mod_page->node, parent, new);
+	rb_insert_color(&mod_page->node, &ick_data->modified_pages_tree);
+	mod_page->orig_page_content = copied_page_content;
+
+	ret = copy_from_user_nofault(copied_page_content, (void *)page_addr, PAGE_SIZE);
+	if (ret) {
+		pr_alert("ick: Failed to copy page 0x%px following wp fault: %pe\n", (void*)page_addr, ERR_PTR(ret));
+		spin_unlock(&ick_data->tree_lock);
+		return VM_FAULT_SIGBUS;
+	}
+
+	spin_unlock(&ick_data->tree_lock);
 
 	return 0;
 }
