95949f1a6229490be869b2acbdff0fe1b3d805c6
Tingmao Wang <m@maowtm.org>
2025-09-09 00:02:38 +0100

    landlock: Add API support and docs for the quiet flags
    Adds the UAPI for the quiet flags feature (but not the implementation
    yet).
    
    According to pahole, even after adding the struct access_masks quiet_masks
    in struct landlock_hierarchy, the u32 log_* bitfield still only has a size
    of 2 bytes, so there's minimal wasted space.
    
    Signed-off-by: Tingmao Wang <m@maowtm.org>
    ---
    
    Changes in v8:
    - The new Landlock ABI version is now v10 as a result of rebase.
    
    Changes in v6:
    - Fix typo in doc
    
    Changes in v5:
    - Doc fixes.
    - Fix build failure without CONFIG_AUDIT / CONFIG_INET (reported by Justin
      Suess)
    
    Changes in v4:
    - Minor update to this commit message.
    - Fix minor formatting
    
    Changes in v3:
    - Updated docs from Mickaël's suggestions.
    
    Changes in v2:
    - Per suggestion, added support for quieting only certain access bits,
      controlled by extra quiet_access_* fields in the ruleset_attr.
    - Added docs for the extra fields and made updates to doc changes in v1.
      In particular, call out that the effect of LANDLOCK_ADD_RULE_QUIET is
      independent from the access bits passed in rule_attr
    - landlock_add_rule will return -EINVAL when LANDLOCK_ADD_RULE_QUIET is
      used but the ruleset does not have any quiet access bits set for the
      given rule type.
    - ABI version bump to v8
    - Syntactic and comment changes per suggestion.

--- security/landlock/ruleset.h
+++ security/landlock/ruleset.h
@@ -172,8 +172,8 @@ struct landlock_ruleset {
 		 * @work_free: Enables to free a ruleset within a lockless
 		 * section.  This is only used by
 		 * landlock_put_ruleset_deferred() when @usage reaches zero.
-		 * The fields @lock, @usage, @num_rules, @num_layers and
-		 * @access_masks are then unused.
+		 * The fields @lock, @usage, @num_rules, @num_layers, @quiet_masks
+		 * and @access_masks are then unused.
 		 */
 		struct work_struct work_free;
 		struct {
@@ -199,6 +199,12 @@ struct landlock_ruleset {
 			 * non-merged ruleset (i.e. not a domain).
 			 */
 			u32 num_layers;
+			/**
+			 * @quiet_masks: Stores the quiet flags for an unmerged
+			 * ruleset.  For a merged domain, this is stored in each
+			 * layer's struct landlock_hierarchy instead.
+			 */
+			struct access_masks quiet_masks;
 			/**
 			 * @access_masks: Contains the subset of filesystem and
 			 * network actions that are restricted by a ruleset.
@@ -229,7 +235,7 @@ DEFINE_FREE(landlock_put_ruleset, struct landlock_ruleset *,
 
 int landlock_insert_rule(struct landlock_ruleset *const ruleset,
 			 const struct landlock_id id,
-			 const access_mask_t access);
+			 const access_mask_t access, const int flags);
 
 struct landlock_ruleset *
 landlock_merge_ruleset(struct landlock_ruleset *const parent,
