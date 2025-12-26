8d5c86a0e6190c58d6cf6428f3264c3356764bcd
Tingmao Wang <m@maowtm.org>
2025-12-26 23:43:07 +0000

    doc

--- tools/testing/selftests/landlock/common.h
+++ tools/testing/selftests/landlock/common.h
@@ -244,11 +244,12 @@ struct service_fixture {
 #define PATHNAME_UNIX_SOCK_DIR TMP_DIR "/pathname_unix"
 
 /**
- * set_unix_address - Set up a UNIX socket address
+ * set_unix_address - Set up srv->unix_addr and srv->unix_addr_len
  * @srv: Service fixture containing the socket address to initialize
- * @index: Unique index for generating distinct socket names
- * @abstract: If true, creates an abstract socket address (sun_path[0] == '\0').
- *            If false, creates a pathname socket address (filesystem path).
+ * @index: Index to include in socket names
+ * @abstract: If true, creates an abstract socket address (sun_path[0] ==
+ *     '\0') with the given name.  If false, creates a pathname socket
+ *     address with the given path.
  */
 static void __maybe_unused set_unix_address(struct service_fixture *const srv,
 					    const unsigned short index,
