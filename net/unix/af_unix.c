300103f7d5ea2a510f73973b8262ce1b2e7c134e
Justin Suess <utilityemal77@gmail.com>
2026-01-19 21:34:54 +0100

    lsm: Add hook security_unix_find
    Add an LSM hook security_unix_find.
    
    This hook is called to check the path of a named unix socket before a
    connection is initiated.
    
    Existing socket hooks, security_unix_stream_connect(),
    security_unix_may_send(), and security_socket_connect() don't provide
    TOCTOU-free / namespace independent access to the paths of sockets.
    
    Why existing hooks are unsuitable:
    
    (1) We cannot resolve the path from the struct sockaddr in existing hooks.
    This requires another path lookup. A change in the path between the
    two lookups will cause a TOCTOU bug.
    
    (2) We cannot use the struct path from the listening socket, because it
    may be bound to a path in a different namespace than the caller,
    resulting in a path that cannot be referenced at policy creation time.
    
    Cc: Günther Noack <gnoack3000@gmail.com>
    Signed-off-by: Justin Suess <utilityemal77@gmail.com>
    Link: https://lore.kernel.org/r/20260119203457.97676-4-gnoack3000@gmail.com

--- net/unix/af_unix.c
+++ net/unix/af_unix.c
@@ -1226,6 +1226,15 @@ static struct sock *unix_find_bsd(struct sockaddr_un *sunaddr, int addr_len,
 	if (!S_ISSOCK(inode->i_mode))
 		goto path_put;
 
+	/*
+	 * We call the hook because we know that the inode is a socket
+	 * and we hold a valid reference to it via the path.
+	 */
+	err = security_unix_find(&path, type, flags);
+	if (err)
+		goto path_put;
+
+	err = -ECONNREFUSED;
 	sk = unix_find_socket_byinode(inode);
 	if (!sk)
 		goto path_put;
